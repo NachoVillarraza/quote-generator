64d4928fae6c2daf152fe8d770a8aae2877e1426
Nacho <nvillarraza@gmail.com>
2021-11-06 20:20:48 -0300

    Proyecto Completado y listo para subir

--- script.js
+++ script.js
@@ -4,23 +4,47 @@ const newQuoteBtn = document.querySelector('#new-quote');
 const twitterBtn = document.querySelector('#twitter');
 const quoteText = document.querySelector('#quote');
 const authorText = document.querySelector('#author');
-
-//Events Listeneres
-newQuoteBtn.addEventListener('click', randomQuote);
+const loader = document.querySelector('#loader');
 
 let quotes = [];
 
+// funciones para el spinner
+function loadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function completeSpinner() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // randomQuote
 function randomQuote() {
+  loadingSpinner();
   let randomNumber = Math.floor(Math.random() * (quotes.length - 1));
   const singleQuote = quotes[randomNumber];
+
+  // Check si contiene autor
+  if (!singleQuote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = singleQuote.author;
+  }
+  // Check el largo de la frase
+  if (singleQuote.text.length > 100) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
   quoteText.textContent = singleQuote.text;
-  authorText.textContent = singleQuote.author;
+  completeSpinner();
 }
 
 // get quotes
 
 async function getQuotes() {
+  loadingSpinner();
   const url = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(url);
@@ -30,6 +54,18 @@ async function getQuotes() {
     alert(error);
   }
 }
+
+// tweetear la frase
+
+function tweetQuote() {
+  const tweetUrl = `https://twitter.com/intent/tweet?text= "${quoteText.textContent}" - ${authorText.textContent}`;
+  window.open(tweetUrl, '_blank');
+}
+
+//Events Listeneres
+newQuoteBtn.addEventListener('click', randomQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 getQuotes();
 
 // fetch(url)
